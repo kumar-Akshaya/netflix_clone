74cd31eac52b417e6c392c23bb09acf431c693c1
kumar-Akshaya <akshayidli1430@gmail.com>
2021-03-23 15:07:53 +0530

    TMDB api keys and Url added.

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "e686d0fe21f104f39f6ad0c6c4f6002a";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_ke:${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
